e5a49f6fbede2cb4a6b9dc13ba4e2ab1e0274b26
Alex Kornitzer <a.kornitzer@me.com>
2016-03-01 20:33:39 +0000

    Added the extra files to the metasploit formula

--- Formula/metasploit.rb
+++ Formula/metasploit.rb
@@ -4,13 +4,32 @@ class Metasploit < Formula
   head "https://github.com/rapid7/metasploit-framework.git"
 
   stable do
-    url "http://updates.metasploit.com/data/releases/framework-3.7.2.tar.bz2"
+    url "http://downloads.metasploit.com/data/releases/framework-4.11.4-2015102801.tar.bz2"
     sha1 'b12991d879d7eb664ffd8f72e4fa11611f10a07d'
   end
 
+  # Resources
+
+
   # Add the depends on
+
+
   def install
-    libexec.install Dir["msf*",'data','external','lib','modules','plugins','scripts','test','tools']
+    libexec.install Dir["msf*",
+                        'config',
+                        'data',
+                        'db',
+                        'documentation',
+                        'external',
+                        'features',
+                        'lib',
+                        'modules',
+                        'plugins',
+                        'script',
+                        'scripts',
+                        'spec',
+                        'test',
+                        'tools']
     bin.mkpath
     Dir["#{libexec}/msf*"].each {|f| ln_s f, bin}
   end
